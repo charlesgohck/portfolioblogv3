88b3dbba5a9e33a6cd0be773fd343af2a28f4cb0
Charles Goh (伍长康) <15852016+charlesgohck@users.noreply.github.com>
2024-08-24 16:39:44 +0800

    [PORTFOLIO] Added MBA to portfolio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,6 +70,7 @@ export default function Home() {
                         <div className="timeline-middle">
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
                         </div>
+                        <div className="timeline-end timeline-box">Master of Business Administration</div>
                     </li>
                 </ul>
             </div>
@@ -85,6 +86,12 @@ export default function Home() {
                     <p className="text-primary"><strong>Master of Science</strong></p>
                     <p className="text-tertiary"><strong>Georgia Institute of Technology</strong></p>
                     <p>Computer Science Major with Interactive Intelligence Specialization</p>
+                </div>
+                <div className="three-element-individual">
+                    <i className="fa-solid fa-briefcase fa-5x"></i>
+                    <p className="text-primary"><strong>Master of Business Administration</strong></p>
+                    <p className="text-tertiary"><strong>Univerisity of Illinois-Urbana Champaign</strong></p>
+                    <p>Focus Area To Be Decided</p>
                 </div>
             </div>
         </main>
