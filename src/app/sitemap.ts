9a10d56c401b3472d19f046f8ac066275222d565
Charles Goh (伍长康) <15852016+charlesgohck@users.noreply.github.com>
2024-12-18 11:25:36 +0800

    [BUGFIX] Added force-static dynamic export for sitemap to fix build problem

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from "next";
 import BlogPostIndex from './../../posts/PostIndex';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
     const currentDateTime: Date = new Date();
     let siteMapArray: MetadataRoute.Sitemap = [
