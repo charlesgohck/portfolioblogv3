0398f313c3b6c10ce64ef01435222c7aa4afbd0b
Charles Goh (伍长康) <15852016+charlesgohck@users.noreply.github.com>
2025-02-23 16:50:59 +0800

    [BLOG][SEO] SEO update to blog contents

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,7 @@
 import BlogListing from "@/components/BlogListing";
 import { wisp } from "@/lib/wisp";
 import { Metadata } from "next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
     title: 'Charles Goh: Blog',
@@ -12,8 +13,14 @@ export default async function Blog() {
 
     let posts = await wisp.getPosts({ limit: "all" });
 
+    const content: string = posts.posts.map(post => post.title + post.description).join("; ");
+
     return (
         <>
+            <Head>
+                <title>Charles Goh: Blog</title>
+                <meta name="description" content={content} />
+            </Head>
             <BlogListing posts={posts}/>
         </>
     )
