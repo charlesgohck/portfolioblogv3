94fa7e3a6d1e0b33c1f2145d09603fadd2f403eb
charlesgohck <15852016+charlesgohck@users.noreply.github.com>
2025-03-02 16:23:50 +0800

    [VERCEL] Removed vercel analytics and speed insights module in shift to app platform

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 import Toast from '@/components/Toast'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -27,8 +25,6 @@ export default function RootLayout({
         {children}
         <Footer/>
         <Toast />
-        <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   )
